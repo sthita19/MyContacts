81164a3dbad9529b4a1c018981b741c11d7174c0
Sthitaprajna <90553962+sthita19@users.noreply.github.com>
2023-07-06 01:00:27 +0530

    Update main.go

--- golang-api-VerSe/main.go
+++ golang-api-VerSe/main.go
@@ -1,90 +1,98 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
 )
 
+// Contact represents the structure of a contact
 type Contact struct {
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-	PhoneNumber string `json:"phone_number"`
+	ID           string `json:"id"`
+	Name         string `json:"name"`
+	PhoneNumber  string `json:"phone_number"`
+	ProfileImage string `json:"profile_image"`
 }
 
 var contacts []Contact
 
+// GetContactsHandler retrieves all contacts
+func GetContactsHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(contacts)
+}
+
+// GetContactHandler retrieves a specific contact by ID
+func GetContactHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, contact := range contacts {
+		if contact.ID == params["id"] {
+			json.NewEncoder(w).Encode(contact)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(nil)
+}
+
+// CreateContactHandler creates a new contact
+func CreateContactHandler(w http.ResponseWriter, r *http.Request) {
+	var contact Contact
+	_ = json.NewDecoder(r.Body).Decode(&contact)
+	contact.ID = generateID()
+	contacts = append(contacts, contact)
+	json.NewEncoder(w).Encode(contact)
+}
+
+// UpdateContactHandler updates an existing contact
+func UpdateContactHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for i, contact := range contacts {
+		if contact.ID == params["id"] {
+			var updatedContact Contact
+			_ = json.NewDecoder(r.Body).Decode(&updatedContact)
+			updatedContact.ID = contact.ID
+			contacts[i] = updatedContact
+			json.NewEncoder(w).Encode(updatedContact)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(nil)
+}
+
+// DeleteContactHandler deletes a contact
+func DeleteContactHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for i, contact := range contacts {
+		if contact.ID == params["id"] {
+			contacts = append(contacts[:i], contacts[i+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(contacts)
+}
+
+func generateID() string {
+	// Generate a unique ID for a contact (you can use your preferred ID generation method)
+	// This is a simple example that uses the length of the contacts slice as the ID
+	return string(len(contacts) + 1)
+}
+
 func main() {
-	router := gin.Default()
+	router := mux.NewRouter()
 
-	// Get all contacts
-	router.GET("/contacts", func(c *gin.Context) {
-		c.JSON(http.StatusOK, contacts)
+	contacts = append(contacts, Contact{
+		ID:           generateID(),
+		Name:         "John Doe",
+		PhoneNumber:  "1234567890",
+		ProfileImage: "profile.jpg",
 	})
 
-	// Get a single contact
-	router.GET("/contacts/:id", func(c *gin.Context) {
-		id := c.Param("id")
+	router.HandleFunc("/contacts", GetContactsHandler).Methods("GET")
+	router.HandleFunc("/contacts/{id}", GetContactHandler).Methods("GET")
+	router.HandleFunc("/contacts", CreateContactHandler).Methods("POST")
+	router.HandleFunc("/contacts/{id}", UpdateContactHandler).Methods("PUT")
+	router.HandleFunc("/contacts/{id}", DeleteContactHandler).Methods("DELETE")
 
-		for _, contact := range contacts {
-			if contact.ID == id {
-				c.JSON(http.StatusOK, contact)
-				return
-			}
-		}
-
-		c.JSON(http.StatusNotFound, gin.H{"error": "Contact not found"})
-	})
-
-	// Create a contact
-	router.POST("/contacts", func(c *gin.Context) {
-		var contact Contact
-
-		if err := c.ShouldBindJSON(&contact); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		contacts = append(contacts, contact)
-		c.Status(http.StatusCreated)
-	})
-
-	// Update a contact
-	router.PUT("/contacts/:id", func(c *gin.Context) {
-		id := c.Param("id")
-
-		var updatedContact Contact
-
-		if err := c.ShouldBindJSON(&updatedContact); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		for i, contact := range contacts {
-			if contact.ID == id {
-				contacts[i] = updatedContact
-				c.Status(http.StatusOK)
-				return
-			}
-		}
-
-		c.JSON(http.StatusNotFound, gin.H{"error": "Contact not found"})
-	})
-
-	// Delete a contact
-	router.DELETE("/contacts/:id", func(c *gin.Context) {
-		id := c.Param("id")
-
-		for i, contact := range contacts {
-			if contact.ID == id {
-				contacts = append(contacts[:i], contacts[i+1:]...)
-				c.Status(http.StatusOK)
-				return
-			}
-		}
-
-		c.JSON(http.StatusNotFound, gin.H{"error": "Contact not found"})
-	})
-
-	router.Run(":8000")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
